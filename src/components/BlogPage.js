bdbc28eca568ced1785bc28e5ba178000a4714dd
chike ibekwe <chifavz@gmail.com>
2025-09-10 22:43:43 -0500

    fix

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,28 +1,24 @@
-// BlogPage.js
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import IndividualBlog from './IndividualBlog'; // Adjust the import path
+import React from "react";
 
 const BlogPage = () => (
   <div>
     <h2>Blog Page</h2>
-   
-
-    <Routes>
-      <Route path="/" element={<div>Select a blog post</div>} />
-      <Route path=":slug" element={<IndividualBlog />} />
-    </Routes>
-    <hr />
-    <MDXProvider components={{ wrapper: BlogPost }}>
-        <Route path={`${match.path}/:slug`}>
-          <BlogPost />
-        </Route>
-      </MDXProvider>
-
+    <p>
+      Visit my affiliate blog for reviews, tutorials, and guides:
+    </p>
+    <a
+      href="https://sparkly-semolina-6f0daa.netlify.app"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="btn-blog"
+    >
+      Go to Blog
+    </a>
   </div>
 );
 
 export default BlogPage;
+
 
 
 
